055957ba83e6370bfead03c5b02ae01c625b3f4f
Hüsler Etienne <etienne.husler@he-arc.ch>
2018-12-20 13:45:11 +0100

    Methods to implement added

--- Otello/Otello/Board.cs
+++ Otello/Otello/Board.cs
@@ -3,11 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IPlayable;
 
 namespace Otello
 {
-    class Board
+    class Board : IPlayable.IPlayable
     {
+        public int GetBlackScore()
+        {
+            throw new NotImplementedException();
+        }
 
+        public int[,] GetBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, int> GetNextMove(int[,] game, int level, bool whiteTurn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetWhiteScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsPlayable(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool PlayMove(int column, int line, bool isWhite)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
