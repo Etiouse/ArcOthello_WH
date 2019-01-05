6ce531415a8f2ff3f1c82eeee53a6ba010ab9ca2
Alexander Wohlfahrt <alexander.wohlfahrt@he-arc.ch>
2019-01-05 16:25:13 +0100

    Create Player and Game classes

---
+++ Otello/Otello/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otello
+{
+    class Game
+    {
+        public Player Player1 { get; private set; }
+        public Player Player2 { get; private set; }
+
+        public Board Board { get; private set; }
+
+        public Game()
+        {
+            Player1 = new Player(1, "Player1");
+            Player2 = new Player(2, "Player2");
+
+            Board = new Board();
+        }
+    }
+}
